b5f8645b2a200ba69ee012b859110b171ab3c5e1
ayushsachan123 <ayushsachan.in@gmail.com>
2022-12-26 18:37:32 +0530

    adding index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+$(".plane1").on("click",function(){
+    $(".plane1").addClass("small-title");
+});
+
+$(".plane2").on("click",function(){
+    $(".plane2").addClass("small-title");
+});
+$(".plane3").on("click",function(){
+    $(".plane3").addClass("small-title");
+});
+$(".plane4").on("click",function(){
+    $(".plane4").addClass("small-title");
+});
